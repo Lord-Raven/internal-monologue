53641271d5d4b519340e5b0eb0c2b9a15375a539
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-05-18 23:03:51 +0000

    Rough draft.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -53,6 +53,10 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
      ***/
     myInternalState: {[key: string]: any};
 
+    currentMonologue: string;
+    monologuePrompt: string;
+    considerPrompt: string;
+
     constructor(data: InitialData<InitStateType, ChatStateType, MessageStateType, ConfigType>) {
         /***
          This is the first thing called in the stage,
@@ -71,9 +75,11 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
             initState,                             // @type: null | InitStateType
             chatState                              // @type: null | ChatStateType
         } = data;
-        this.myInternalState = messageState != null ? messageState : {'someKey': 'someValue'};
-        this.myInternalState['numUsers'] = Object.keys(users).length;
-        this.myInternalState['numChars'] = Object.keys(characters).length;
+        this.myInternalState = {};
+        this.myInternalState['messageState'] = messageState ?? '';
+        this.currentMonologue = '';
+        this.monologuePrompt = 'Summarize the internal thoughts of other characters in the scene, based on their personalities and ongoing events.';
+        this.considerPrompt = `These are the internal thoughts of characters in the scene: ${this.currentMonologue}\nSilently consider the internal thoughts of each character when depicting their actions or dialog.`
     }
 
     async load(): Promise<Partial<LoadResponse<InitStateType, ChatStateType, MessageStateType>>> {
@@ -102,9 +108,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
          or a swipe. Note how neither InitState nor ChatState are given here. They are not for
          state that is affected by swiping.
          ***/
-        if (state != null) {
-            this.myInternalState = {...this.myInternalState, ...state};
-        }
+        this.myInternalState['messageState'] = state ?? '';
     }
 
     async beforePrompt(userMessage: Message): Promise<Partial<StageResponse<ChatStateType, MessageStateType>>> {
@@ -120,13 +124,17 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
             isBot             /*** @type: boolean
              @description Whether this is itself from another bot, ex. in a group chat. ***/
         } = userMessage;
+        await this.generator.textGen({
+            prompt: this.monologuePrompt,
+            max_tokens: 100,
+            include_history: true}).then(message => this.currentMonologue = message ? message.result : '');
         return {
             /*** @type null | string @description A string to add to the
              end of the final prompt sent to the LLM,
              but that isn't persisted. ***/
-            stageDirections: null,
+            stageDirections: this.considerPrompt,
             /*** @type MessageStateType | null @description the new state after the userMessage. ***/
-            messageState: {'someKey': this.myInternalState['someKey']},
+            messageState: this.currentMonologue,
             /*** @type null | string @description If not null, the user's message itself is replaced
              with this value, both in what's sent to the LLM and in the database. ***/
             modifiedMessage: null,
@@ -162,7 +170,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
              but that isn't persisted. ***/
             stageDirections: null,
             /*** @type MessageStateType | null @description the new state after the botMessage. ***/
-            messageState: {'someKey': this.myInternalState['someKey']},
+            messageState: this.currentMonologue,
             /*** @type null | string @description If not null, the bot's response itself is replaced
              with this value, both in what's sent to the LLM subsequently and in the database. ***/
             modifiedMessage: null,
@@ -195,10 +203,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
             display: 'grid',
             alignItems: 'stretch'
         }}>
-            <div>Hello World! I'm an empty stage! With {this.myInternalState['someKey']}!</div>
-            <div>There is/are/were {this.myInternalState['numChars']} character(s)
-                and {this.myInternalState['numUsers']} human(s) here.
-            </div>
+            <div>Current monologue: {this.myInternalState['messageState']}</div>
         </div>;
     }
 
