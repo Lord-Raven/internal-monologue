da2bbc8d4b7ca1d55ce8c1fc7a18ae14fe5a304d
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2024-06-17 23:17:59 -0400

    Track history and build prompt.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -1,64 +1,32 @@
 import {ReactElement} from "react";
 import {StageBase, StageResponse, InitialData, Message} from "@chub-ai/stages-ts";
 import {LoadResponse} from "@chub-ai/stages-ts/dist/types/load";
-import {Character} from "@chub-ai/stages-ts";
+import {Character, User} from "@chub-ai/stages-ts";
 
-/***
- The type that this stage persists message-level state in.
- This is primarily for readability, and not enforced.
-
- @description This type is saved in the database after each message,
-  which makes it ideal for storing things like positions and statuses,
-  but not for things like history, which is best managed ephemerally
-  in the internal state of the Stage class itself.
- ***/
 type MessageStateType = any;
 
-/***
- The type of the stage-specific configuration of this stage.
-
- @description This is for things you want people to be able to configure,
-  like background color.
- ***/
 type ConfigType = any;
 
-/***
- The type that this stage persists chat initialization state in.
- If there is any 'constant once initialized' static state unique to a chat,
- like procedurally generated terrain that is only created ONCE and ONLY ONCE per chat,
- it belongs here.
- ***/
 type InitStateType = any;
 
-/***
- The type that this stage persists dynamic chat-level state in.
- This is for any state information unique to a chat,
-    that applies to ALL branches and paths such as clearing fog-of-war.
- It is usually unlikely you will need this, and if it is used for message-level
-    data like player health then it will enter an inconsistent state whenever
-    they change branches or jump nodes. Use MessageStateType for that.
- ***/
 type ChatStateType = any;
 
-/***
- A simple example class that implements the interfaces necessary for a Stage.
- If you want to rename it, be sure to modify App.js as well.
- @link https://github.com/CharHubAI/chub-stages-ts/blob/main/src/types/stage.ts
- ***/
 export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateType, ConfigType> {
 
-    /***
-     A very simple example internal state. Can be anything.
-     This is ephemeral in the sense that it isn't persisted to a database,
-     but exists as long as the instance does, i.e., the chat page is open.
-     ***/
-    myInternalState: {[key: string]: any};
+    readonly monologuePrompt: string = '[Rather than continue the narration, use this response to transcribe a couple brief sentences of {{char}}\'s current first-person thoughts about the past few moments of the scene, shaped by personality, motives, and recent events. Describe their honest opinions and the reactions they are considering in the moment]';
 
-    chatId: number;
+    // chatState
+    messageParentIds: {[key: string]: string};
+    messageBodies: {[key: string]: string};
 
+    // messageState
     monologues: {[key: string]: string};
+    messageId: string;
+
+    // other
     characters: {[key: string]: Character};
-    readonly monologuePrompt: string = '[Rather than continue the narration, use this response to transcribe a couple brief sentences of {{char}}\'s current first-person thoughts about the scene in this exact moment, shaped by personality, motives, and recent events.]';
+    user: User;
+
 
     formatPrompt(characterId: string|null): string {
         return (!characterId || !this.monologues[characterId]) ? '' :
@@ -66,99 +34,107 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
     }
 
     constructor(data: InitialData<InitStateType, ChatStateType, MessageStateType, ConfigType>) {
-        /***
-         This is the first thing called in the stage,
-         to create an instance of it.
-         The definition of InitialData is at @link https://github.com/CharHubAI/chub-stages-ts/blob/main/src/types/initial.ts
-         Character at @link https://github.com/CharHubAI/chub-stages-ts/blob/main/src/types/character.ts
-         User at @link https://github.com/CharHubAI/chub-stages-ts/blob/main/src/types/user.ts
-         ***/
+
         super(data);
         const {
-            characters,         // @type:  { [key: string]: Character }
-            users,                  // @type:  { [key: string]: User}
-            config,                                 //  @type:  ConfigType
-            messageState,                           //  @type:  MessageStateType
-            environment,                     // @type: Environment (which is a string)
-            initState,                             // @type: null | InitStateType
-            chatState                              // @type: null | ChatStateType
+            characters,
+            users,
+            config,
+            messageState,
+            chatState
         } = data;
-        this.myInternalState = {};
-        this.monologues = messageState ?? {};
         this.characters = characters;
+        this.user = users[Object.keys(users)[0]];
         this.monologues = {};
-        this.chatId = this.getChatId();
+        this.messageId = '';
+        this.messageParentIds = {};
+        this.messageBodies = {};
+        this.readChatState(chatState);
+        this.readMessageState(messageState);
     }
 
     async load(): Promise<Partial<LoadResponse<InitStateType, ChatStateType, MessageStateType>>> {
-        /***
-         This is called immediately after the constructor, in case there is some asynchronous code you need to
-         run on instantiation.
-         ***/
+
         return {
-            /*** @type boolean @default null
-             @description The 'success' boolean returned should be false IFF (if and only if), some condition is met that means
-              the stage shouldn't be run at all and the iFrame can be closed/removed.
-              For example, if a stage displays expressions and no characters have an expression pack,
-              there is no reason to run the stage, so it would return false here. ***/
             success: true,
-            /*** @type null | string @description an error message to show
-             briefly at the top of the screen, if any. ***/
             error: null,
             initState: null,
-            chatState: null,
+            chatState: this.writeChatState(),
         };
     }
 
-    async setState(state: MessageStateType): Promise<void> {
-        /***
-         This can be called at any time, typically after a jump to a different place in the chat tree
-         or a swipe. Note how neither InitState nor ChatState are given here. They are not for
-         state that is affected by swiping.
-         ***/
-        this.monologues = state ?? {};
+    readMessageState(messageState: MessageStateType) {
+        this.monologues = messageState.monologues ?? {};
+        this.messageId = messageState.messageId ?? '';
+    }
+
+    writeMessageState(): MessageStateType {
+        return {
+            monologues: this.monologues,
+            messageId: this.messageId
+        };
+    }
+
+    readChatState(chatState: ChatStateType) {
+        this.messageBodies = chatState.messageBodies ?? {};
+        this.messageParentIds = chatState.messageParentIds ?? {};
+    }
+
+    writeChatState(): ChatStateType {
+        return {
+            messageBodies: this.messageBodies,
+            messageParentIds: this.messageParentIds
+        };
+    }
+
+    buildHistory(messageId: string): string {
+        let currentId = messageId;
+        let historyString = this.messageBodies[currentId] ?? '';
+        let depth = 0;
+        while(this.messageParentIds[currentId] && depth < 10) {
+            currentId = this.messageParentIds[currentId];
+            historyString = `${historyString}\n\n${this.messageBodies[currentId] ?? ''}`;
+            depth++;
+        }
+
+        return historyString;
+    }
+
+    async setState(messageState: MessageStateType): Promise<void> {
+        this.readMessageState(messageState);
     }
 
     async beforePrompt(userMessage: Message): Promise<Partial<StageResponse<ChatStateType, MessageStateType>>> {
-        /***
-         This is called after someone presses 'send', but before anything is sent to the LLM.
-         ***/
+
         const {
-            content,            /*** @type: string
-             @description Just the last message about to be sent. ***/
-            anonymizedId,       /*** @type: string
-             @description An anonymized ID that is unique to this individual
-              in this chat, but NOT their Chub ID. ***/
-            isBot,              /*** @type: boolean
-             @description Whether this is itself from another bot, ex. in a group chat. ***/
-            promptForId,        /*** @type: string
-             @description The anonymized ID of the bot or human being prompted, if any.
-              Essentially only relevant to beforePrompt currently. ***/
-            identity            /*** @type string
-             @description The unique ID of this chat message. ***/
+            content,
+            isBot,
+            promptForId,
+            identity
         } = userMessage;
         console.log('testing: ' + content + ';' + isBot + ';' + promptForId + ';' + identity);
-        if (promptForId) {
-
-            /*console.log('Attempting API call.');
-            let response: object = await this.getApiResponse(`https://api.chub.ai/api/venus/chats/v2/${this.getChatId()}/messages/main`, 
-                {
-                    "message_ids": [identity],
-                    "set": true
-                });
-            console.log('messenger complete');*/
+        this.messageParentIds[identity] = this.messageId;
+        this.messageId = identity;
+        this.messageBodies[identity] = `###Input: ${this.user.name}: ${content}`;
+        if (promptForId && this.characters[promptForId] && promptForId != this.user.anonymizedId) {
+            console.log('generating');
+            // Build monologue prompt:
+            const promptedCharacter = this.characters[promptForId];
+            const history = this.buildHistory(this.messageId);
+            let monologuePrompt = `[INST]\n### Instruction:\n${promptedCharacter.system_prompt}\n` +
+                `About ${promptedCharacter.name}: ${promptedCharacter.description}\n${promptedCharacter.personality}\n` +
+                `Circumstances and context of the dialogue: ${promptedCharacter.scenario}\n` +
+                `About ${this.user.name}: ${this.user.chatProfile}\n` +
+                `[/INST]\n${history}\n${promptedCharacter.post_history_instructions}\n` +
+                `${this.monologuePrompt}`;
 
             let result = await this.generator.textGen({
-                prompt: this.monologuePrompt,
-                min_tokens: 10,
-                max_tokens: 100,
-                stop: [],
-                include_history: true,
-                template: '{{personality}};{{memory}};{{profile}};{{summary}}',
-                context_length: 2500});
+                prompt: monologuePrompt,
+                min_tokens: 50,
+                max_tokens: 200
+            });
             if (result) {
                 console.log('result:' + result.result);
-
             } else {
                 console.log('no result');
             }
@@ -166,25 +142,12 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
         }
         console.log('after: ' + promptForId + ":" + this.formatPrompt(promptForId));
         return {
-            /*** @type null | string @description A string to add to the
-             end of the final prompt sent to the LLM,
-             but that isn't persisted. ***/
             stageDirections: this.formatPrompt(promptForId),
-            /*** @type MessageStateType | null @description the new state after the userMessage. ***/
-            messageState: this.monologues,
-            /*** @type null | string @description If not null, the user's message itself is replaced
-             with this value, both in what's sent to the LLM and in the database. ***/
+            messageState: this.writeMessageState(),
             modifiedMessage: null,
-            /*** @type null | string @description A system message to append to the end of this message.
-             This is unique in that it shows up in the chat log and is sent to the LLM in subsequent messages,
-             but it's shown as coming from a system user and not any member of the chat. If you have things like
-             computed stat blocks that you want to show in the log, but don't want the LLM to start trying to
-             mimic/output them, they belong here. ***/
             systemMessage: null,
-            /*** @type null | string @description an error message to show
-             briefly at the top of the screen, if any. ***/
             error: null,
-            chatState: null,
+            chatState: this.writeChatState(),
         };
     }
 
@@ -193,104 +156,30 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
          This is called immediately after a response from the LLM.
          ***/
         const {
-            content,            /*** @type: string
-             @description The LLM's response. ***/
-            anonymizedId,       /*** @type: string
-             @description An anonymized ID that is unique to this individual
-              in this chat, but NOT their Chub ID. ***/
-            isBot             /*** @type: boolean
-             @description Whether this is from a bot, conceivably always true. ***/
+            identity,
+            content,
+            anonymizedId,
+            isBot
         } = botMessage;
 
         console.log('testing2: ' + content + ';' + isBot);
-        let result = await this.generator.textGen({
-            prompt: this.monologuePrompt,
-            stop: [],
-            max_tokens: 100,
-            include_history: true});
-        this.monologues[anonymizedId] = result ? result.result : '';
-
-        /*this.generator.textGen({
-            prompt: this.monologuePrompt,
-            max_tokens: 100,
-            include_history: true}).then(result => this.currentMonologue = result ? result.result : '');*/
+        this.messageParentIds[identity] = this.messageId;
+        this.messageId = identity;
+        this.messageBodies[identity] = `###Response: ${this.characters[anonymizedId]}: ${content}`;
 
         return {
-            /*** @type null | string @description A string to add to the
-             end of the final prompt sent to the LLM,
-             but that isn't persisted. ***/
             stageDirections: null,
-            /*** @type MessageStateType | null @description the new state after the botMessage. ***/
-            messageState: this.monologues,
-            /*** @type null | string @description If not null, the bot's response itself is replaced
-             with this value, both in what's sent to the LLM subsequently and in the database. ***/
+            messageState: this.writeMessageState(),
             modifiedMessage: null,
-            /*** @type null | string @description an error message to show
-             briefly at the top of the screen, if any. ***/
             error: null,
             systemMessage: null,
-            chatState: null
+            chatState: this.writeChatState()
         };
     }
 
-    getChatId(): number {
-        /*if (!this.chatId || this.chatId <= 0) {
-            const url = window.parent.location.href;
-            console.log('url:' + url);
-            const match = url.match(/(\d+)(?!.*\d)/);
-            this.chatId = match ? parseInt(match[1], 10) : -1;
-            console.log('Found ID?' + this.chatId);
-        }*/
-
-        return -1;//this.chatId;
-    }
-
-    async getApiResponse(url: string, body: object): Promise<object> {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(body),
-            mode: 'cors'
-        });
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        return response.json() as Promise<object>;
-      }
     
     render(): ReactElement {
-        /***
-         There should be no "work" done here. Just returning the React element to display.
-         If you're unfamiliar with React and prefer video, I've heard good things about
-         @link https://scrimba.com/learn/learnreact but haven't personally watched/used it.
-
-         For creating 3D and game components, react-three-fiber
-           @link https://docs.pmnd.rs/react-three-fiber/getting-started/introduction
-           and the associated ecosystem of libraries are quite good and intuitive.
-
-         Cuberun is a good example of a game built with them.
-           @link https://github.com/akarlsten/cuberun (Source)
-           @link https://cuberun.adamkarlsten.com/ (Demo)
-         ***/
-        return <div style={{
-            width: '100vw',
-            height: '100vh',
-            display: 'grid',
-            alignItems: 'stretch'
-        }}>
-            <h1>Characters</h1>
-            <ul>
-                {Object.keys(this.characters).map(key => (
-                    <li key={key}>
-                        {this.characters[key].name}: {this.monologues[key]}
-                    </li>
-                ))}
-            </ul>
-        </div>;
+        return (<div></div>);
     }
 
 }
