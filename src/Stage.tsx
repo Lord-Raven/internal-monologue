902603b638a16d23488d3904387bf8910b271077
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2025-11-25 13:38:02 +0000

    Minor fix. First generateMonologue() call was replacing char tag in the canon generationPrompt rather than just for this request.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -88,12 +88,11 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
             // Build monologue prompt:
             const char = this.characters[characterId];
             const user = this.users[userId];
-            this.generationPrompt = this.replaceTags(this.generationPrompt, {'char': char.name})
             let monologuePrompt = `{{system_prompt}}\n\n` +
                 `About ${char.name}:\n${char.description} ${char.personality}\n\n` +
                 `About {{user}}:\n${user.chatProfile}\n\n` +
                 `Conversation history:\n{{messages}}\n${historyAddition}\n` + // Potentially include new message from user
-                `Current Instruction:\n${this.generationPrompt}\n`
+                `Current Instruction:\n${this.replaceTags(this.generationPrompt, {'char': char.name})}\n`
 
             console.log('Monologue prompt:\n' + monologuePrompt);
             let result: TextResponse|null = await this.generator.textGen({
